3440e408b1929cf98d6b9dc6a8ee8255694def42
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-03 11:24:12 +0000

    firebase config

--- utils/firebase.js
+++ utils/firebase.js
@@ -7,14 +7,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAGVmcTKVZwUqJpOI9Zoah9rbofyiQvd4",
-  authDomain: "creative-ideas-19591.firebaseapp.com",
-  projectId: "creative-ideas-19591",
-  storageBucket: "creative-ideas-19591.appspot.com",
-  messagingSenderId: "660757897031",
-  appId: "1:660757897031:web:e09af5c3cb9c72eec7b662"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  // "AIzaSyDAGVmcTKVZwUqJpOI9Zoah9rbofyiQvd4",
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  // "creative-ideas-19591.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // projectId: "creative-ideas-19591",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  // storageBucket: "creative-ideas-19591.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  // messagingSenderId: "660757897031",
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  // appId: "1:660757897031:web:e09af5c3cb9c72eec7b662",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
