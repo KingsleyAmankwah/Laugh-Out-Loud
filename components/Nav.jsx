0eb5de01874785dc5ab89285db13b6a7f269bd9f
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-04 10:54:51 +0000

    Navbar Comonent

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,37 +1,39 @@
 import Link from "next/link";
 import { auth } from "../utils/firebase";
-// import { useAuthState } from "react-firebase-hooks/auth";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { RxAvatar } from "react-icons/rx";
 export default function Nav() {
-  // const [user, loading] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
   return (
     <nav className="flex justify-between items-center py-10">
       <Link href="/">
-        <button className="text-lg font-medium">Creative Ideas</button>
+        <button className="text-lg font-medium">Creative Minds</button>
       </Link>
-
       <ul className="flex items-center gap-10">
-        <Link
-          href={"/auth/login"}
-          className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"
-        >
-          Join Now
-        </Link>
-
-        <div className="flex items-center gap-6">
-          <Link href="/post">
-            <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-sm">
-              Post
-            </button>
+        {!user && (
+          <Link
+            href={"/auth/login"}
+            className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"
+          >
+            Join Now
           </Link>
-          <Link href="/dashboard">
-            <img
-              className="w-12 rounded-full cursor-pointer"
-              // src={user.photoURL}
-            />
-          </Link>
-        </div>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-sm">
+                Post
+              </button>
+            </Link>
+            <Link href="/dashboard">
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                src={user.photoURL || user.email[0]}
+              />
+            </Link>
+          </div>
+        )}
       </ul>
     </nav>
   );
