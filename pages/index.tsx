3dde5b44d96706eaeab610515dd55a18e807f07c
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-04 10:55:39 +0000

    app entry file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-
-
+import Message from "../components/Message";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -14,7 +13,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <div className="my-12 text-lg font-medium">
+          <h2>See what other people are saying</h2>
+        </div>
+      </main>
     </>
   );
 }
