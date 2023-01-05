b6f4a0a4a251cd8de076da45d2ef87d7ce40a947
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-05 04:23:26 +0000

    Post page

--- pages/post.jsx
+++ pages/post.jsx
@@ -2,64 +2,59 @@ import { auth, db } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-// import {
-//   addDoc,
-//   collection,
-//   doc,
-//   serverTimestamp,
-//   updateDoc,
-// } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 import { toast } from "react-toastify";
+import { RiLoader5Line } from "react-icons/ri";
 
 export default function Post() {
   //Form state
   const [post, setPost] = useState({ description: "" });
   const [user, loading] = useAuthState(auth);
+  const [Loading, setLoading] = useState(false);
   const route = useRouter();
   const routeData = route.query;
 
   //Submit Post
-  //   const submitPost = async (e) => {
-  //     e.preventDefault();
-  //     //Run checks for description
-  //     if (!post.description) {
-  //       toast.error("Description Field empty 😅", {
-  //         position: toast.POSITION.TOP_CENTER,
-  //         autoClose: 1500,
-  //       });
-  //       return;
-  //     }
-  //     if (post.description.length > 300) {
-  //       toast.error("Description too long 😅", {
-  //         position: toast.POSITION.TOP_CENTER,
-  //         autoClose: 1500,
-  //       });
-  //       return;
-  //     }
+  const submitPost = async (e) => {
+    e.preventDefault();
+    //Run checks for description
+    if (!post.description) {
+      toast.error("Description Field empty 😅", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
+    }
 
-  //     if (post?.hasOwnProperty("id")) {
-  //       const docRef = doc(db, "posts", post.id);
-  //       const updatedPost = { ...post, timestamp: serverTimestamp() };
-  //       await updateDoc(docRef, updatedPost);
-  //       return route.push("/");
-  //     } else {
-  //       //Make a new post
-  //       const collectionRef = collection(db, "posts");
-  //       await addDoc(collectionRef, {
-  //         ...post,
-  //         timestamp: serverTimestamp(),
-  //         user: user.uid,
-  //         avatar: user.photoURL,
-  //         username: user.displayName,
-  //       });
-  //       setPost({ description: "" });
-  //       toast.success("Post has been made 🚀", {
-  //         position: toast.POSITION.TOP_CENTER,
-  //         autoClose: 1500,
-  //       });
-  //       return route.push("/");
-  //     }
-  //   };
+    if (post?.hasOwnProperty("id")) {
+      const docRef = doc(db, "posts", post.id);
+      const updatedPost = { ...post, timestamp: serverTimestamp() };
+      await updateDoc(docRef, updatedPost);
+      return route.push("/");
+    } else {
+      //Make a new post
+      const collectionRef = collection(db, "posts");
+      await addDoc(collectionRef, {
+        ...post,
+        timestamp: serverTimestamp(),
+        user: user.uid,
+        avatar: user.photoURL,
+        username: user.displayName,
+      });
+      setPost({ description: "" });
+      toast.success("Post has been created 🚀", {
+        // position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return route.push("/");
+    }
+  };
 
   //Check our user
   const checkUser = async () => {
@@ -76,7 +71,7 @@ export default function Post() {
 
   return (
     <div className="my-20 p-12 shadow-2xl rounded-lg max-w-md mx-auto">
-      <form>
+      <form onSubmit={submitPost}>
         <h1 className="text-2xl font-bold">
           {post.hasOwnProperty("id") ? "Edit your post" : "Create a new post"}
         </h1>
@@ -97,10 +92,11 @@ export default function Post() {
         </div>
         <button
           type="submit"
-          className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm disabled:cursor-not-allowed"
+          className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm disabled:bg-cyan-300 disabled:cursor-not-allowed"
           disabled={post.description.length > 300}
         >
-          Submit
+          {Loading && <RiLoader5Line className="animate-spin text-2xl" />}
+          {!Loading ? "Submit" : "Loading..."}
         </button>
       </form>
     </div>
