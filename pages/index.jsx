0cc464179004bf7f160145dbd0dfd8d293f87a6e
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-06 01:40:54 +0000

    Loader added to page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import Message from "../components/Message";
 import { useEffect, useState } from "react";
 import { db } from "../utils/firebase";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import Link from "next/link";
+import Spinner from "../components/Spinner";
 
 export default function Home() {
   const [allPosts, setAllPosts] = useState([]);
+  const [Loading, setLoading] = useState(false);
 
   const getPosts = async () => {
+    setLoading(true);
     const collectionRef = collection(db, "posts");
     const q = query(collectionRef, orderBy("timestamp", "desc"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
       setAllPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false);
     });
+
     return unsubscribe;
   };
 
   useEffect(() => {
     getPosts();
   }, []);
+
+  if (Loading) return <Spinner />;
 
   return (
     <>
@@ -37,12 +43,12 @@ export default function Home() {
 
           {allPosts.map((post) => (
             <Message key={post.id} {...post}>
-              {/* <Link href={{ pathname: `/${post.id}`, query: { ...post } }}>
+              <Link href={{ pathname: `/${post.id}`, query: { ...post } }}>
                 <button>
                   {post.comments?.length > 0 ? post.comments?.length : 0}{" "}
                   comments
                 </button>
-              </Link> */}
+              </Link>
             </Message>
           ))}
         </div>
