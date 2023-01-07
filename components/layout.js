bc5a7ef7a9883bd8ea880c20f3dc4d60bf684ccd
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-01-07 03:11:18 +0000

    Layout component

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="mx-1 md:max-w-2xl md:mx-auto font-[system-ui]">
+        <Nav />
+        <main>{children}</main>
+      </div>
+    </>
+  );
+}
